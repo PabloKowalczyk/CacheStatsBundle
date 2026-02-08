41c24fad135cfb90184e3cf0c9fc9c886231d947
PabloKowalczyk <11366345+PabloKowalczyk@users.noreply.github.com>
2026-02-08 18:03:21 +0100

    Fix Extension deprecation (#37)

--- src/DependencyInjection/PabloKCacheStatsExtension.php
+++ src/DependencyInjection/PabloKCacheStatsExtension.php
@@ -6,8 +6,8 @@ namespace PabloK\CacheStatsBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 final class PabloKCacheStatsExtension extends Extension
 {
